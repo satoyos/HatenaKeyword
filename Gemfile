89cf37ae36ced6ab1c1b49023879165d7eaeb246
Yoshifumi Sato <sato_yos@nifty.com>
2015-02-11 17:49:33 +0900

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rspec'
+gem 'rspec-collection_matchers'
+gem 'nokogiri'
---
+++ spec/daily_keyword_extractor_spec.rb
@@ -0,0 +1,6 @@
+describe 'DailyKeywordExtractor' do
+  let(:extractor){DailyKeywordExtractor.new}
+  it 'should be a valid object' do
+    expect(extractor).not_to be nil
+  end
+end
